63ac343a881aea43bcc608adbfbaee91c4c927ae
Sabina Karlsson <karlsson.sst@gmail.com>
2025-04-02 13:19:05 +0200

    Initialize Resource project

---
+++ BankBlazorApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankBlazorApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
