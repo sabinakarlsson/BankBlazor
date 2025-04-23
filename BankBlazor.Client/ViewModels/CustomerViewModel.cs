5461c04b3cb7d71032c3c90d6cae6770311ebdce
Sabina Karlsson <karlsson.sst@gmail.com>
2025-04-23 11:51:25 +0200

    Added ViewModels

---
+++ BankBlazor.Client/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BankBlazor.Client.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int CustomerId { get; set; }
+        public string Givenname { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public int AccountId { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
--- BankBlazorApi/Program.cs
+++ BankBlazorApi/Program.cs
@@ -1,4 +1,10 @@
 
+using Microsoft.EntityFrameworkCore;
+using BankBlazorApi.Contexts;
+using BankBlazorApi.Services;
+using BankBlazorApi.Services.Interfaces;
+using BankBlazorApi.Data;
+
 namespace BankBlazorApi
 {
     public class Program
@@ -14,7 +20,27 @@ namespace BankBlazorApi
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            /*Initiera NorthwindContext(dbContext) för Dependency Injection*/
+            builder.Services.AddDbContext<BankBlazorContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindContext"));
+            });
+
+            builder.Services.AddScoped<ITransactionService, TransactionService>();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowBlazorClient", policy =>
+                {
+                    policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
+
             var app = builder.Build();
+
+            app.UseCors("AllowBlazorClient");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
